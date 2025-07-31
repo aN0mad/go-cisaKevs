b074e9750247e15f48a31d63051bbf1f1cde5a0e
Ryan Tucker <ryan.tucker@ondefend.com>
2025-07-31 19:21:36 +0000

    Added additional logging levels

--- cisakev/logger.go
+++ cisakev/logger.go
@@ -3,16 +3,20 @@ package cisakev
 import "log"
 
 type Logger interface {
+	Trace(msg string, args ...any)
 	Debug(msg string, args ...any)
 	Info(msg string, args ...any)
 	Warn(msg string, args ...any)
 	Error(msg string, args ...any)
+	Fatal(msg string, args ...any)
 }
 
 // defaultLogger implements the Logger interface
 type defaultLogger struct{}
 
+func (l *defaultLogger) Trace(msg string, args ...any) { log.Println("TRACE:", msg) }
 func (l *defaultLogger) Debug(msg string, args ...any) { log.Println("DEBUG:", msg) }
 func (l *defaultLogger) Info(msg string, args ...any)  { log.Println("INFO:", msg) }
 func (l *defaultLogger) Warn(msg string, args ...any)  { log.Println("WARN:", msg) }
 func (l *defaultLogger) Error(msg string, args ...any) { log.Println("ERROR:", msg) }
+func (l *defaultLogger) Fatal(msg string, args ...any) { log.Fatalln("FATAL:", msg) }
